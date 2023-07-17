f8e156ad51e18af1641a3027ccb067e3be6557bf
DatAsianBoi123 <zach.su158@gmail.com>
2023-07-16 18:44:38 -0600

    Created sendrules command

--- src/main.rs
+++ src/main.rs
@@ -76,6 +76,31 @@ async fn help(ctx: Context<'_>) -> Result {
     Ok(())
 }
 
+#[command(slash_command, rename = "sendrules", required_permissions = "MANAGE_GUILD")]
+async fn send_rules(ctx: Context<'_>) -> Result {
+    ctx.channel_id().send_message(ctx, |m| {
+        m.embed(|embed| {
+            embed
+                .title("Rules")
+                .description("**1. Be Respectful**\
+                             Just be respectful to other guild members.\n\
+                             **2. Limit Profanity**\
+                             Don't spam swear words.\n\
+                             **3. Avoid NSFW Content**\
+                             Do not post any sort of NSFW or controversial topic in any channel.\n\
+                             **4. No Spamming**\
+                             Do not spam random messages.\n\
+                             **5. No Advertising**\
+                             Do not advertise anything in any channel.\n\
+                             **6. English Only**\
+                             Having every message be in English helps us moderate better.")
+                .color(Color::from_rgb(255, 255, 255))
+        })
+    }).await?;
+    ctx.send(|m| m.content("Done!").ephemeral(true)).await?;
+    Ok(())
+}
+
 #[command(
     slash_command,
     description_localized("en-US", "Views the recommended loadout during a specific stage of progression"),
@@ -160,6 +185,7 @@ async fn poise(
                 ping(),
                 view_loadout(),
                 help(),
+                send_rules(),
             ],
             ..Default::default()
         })
